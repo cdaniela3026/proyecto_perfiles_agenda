06b6d399ac9a56ad471ccb73ea9885e7ac39b602
Dennis Adrian <adriangbetancur@gmail.com>
2020-06-29 11:48:53 -0400

    cambios en la bd

--- scriptv2.sql
+++ scriptv2.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS "facultad" (
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS "carrera" (
     "id" INTEGER PRIMARY KEY,
     "nombre" TEXT UNIQUE,
@@ -13,30 +12,26 @@ CREATE TABLE IF NOT EXISTS "carrera" (
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_facultad_carrera FOREIGN KEY ("id_facultad") REFERENCES "facultad"("id")
 );
-
 CREATE TABLE IF NOT EXISTS "institucion" (
     "id" INTEGER PRIMARY KEY,
     "nombre" TEXT NOT NULL,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS "carrera_licenciado" (
     "id" INTEGER PRIMARY KEY,
     "nombre" TEXT NOT NULL,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS "tipo_licenciado"(
+-- nombre modificado y se quito el campo "tipo"
+CREATE TABLE IF NOT EXISTS "funcion_licenciado"(
     "id" INTEGER PRIMARY KEY,
-    "tipo" TEXT COMMENT 'interno, externo',
-    "funcion_licenciado" TEXT COMMENT 'tribunal interno 1 o 2, presidente, secretario, representante del ministrerio de educacion, representante uagrm 1 o 2',
+    "funcion_licenciado" TEXT COMMENT 'tutor, tribunal interno 1 o 2, presidente, secretario, representante del ministrerio de educacion, representante uagrm 1 o 2',
     "descripcion" TEXT,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
 CREATE TABLE IF NOT EXISTS "licenciado" (
     "id" INTEGER PRIMARY KEY,
     "nombre" TEXT NOT NULL,
@@ -45,17 +40,16 @@ CREATE TABLE IF NOT EXISTS "licenciado" (
     "email" TEXT UNIQUE,
     "telefono" TEXT,
     "celular" TEXT,
+    -- campo agregado
+    "tipo" TEXT COMMENT 'interno o externo',
     "docente" NUMERIC COMMENT '0 = no, 1 = si',
-    
     "id_institucion_representada" INTEGER NOT NULL,
     "id_carrera_licenciado" INTEGER NOT NULL,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_institucion_licenciado FOREIGN KEY("id_institucion_representada") REFERENCES "institucion"("id"),
-    
     CONSTRAINT fk_carrera_licenciado FOREIGN KEY("id_carrera_licenciado") REFERENCES "carrera_licenciado"("id")
 );
-
 CREATE TABLE IF NOT EXISTS "estudiante" (
     "id" INTEGER PRIMARY KEY,
     "registro" TEXT NOT NULL,
@@ -72,27 +66,32 @@ CREATE TABLE IF NOT EXISTS "estudiante" (
 CREATE TABLE IF NOT EXISTS "perfil_tesis" (
     "id" INTEGER PRIMARY KEY,
     "tema" TEXT NOT NULL,
-    "estado" TEXT COMMENT 'rechazado, aprobado, en revision, en conclusion, abandonado, etc' NOT NULL,
+    "estado" TEXT COMMENT 'recepcionado, rechazado, aprobado, en revision, abandonado' NOT NULL,
     "fecha_aprobacion_jefe_carrera" TEXT,
     "fecha_recepcion_titulacion" TEXT,
     "estado_defensa" TEXT COMMENT 'aprobada o reprobada',
     "calificacion" REAL,
     "id_estudiante" INTEGER NOT NULL,
     "id_licenciado" INTEGER COMMENT 'tutor de tesis' NOT NULL,
-    "id_tipo_licenciado" INTEGER,
+    "id_funcion_licenciado" INTEGER,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_tutor_perfiltesis FOREIGN KEY("id_licenciado") REFERENCES "licenciado"("id"),
-    FOREIGN key ("id_tipo_licenciado") REFERENCES "tipo_licenciado"("id"),
-    FOREIGN key ("id_estudiante")REFERENCES "estudiante" ("id")
+    FOREIGN key ("id_funcion_licenciado") REFERENCES "funcion_licenciado"("id"),
+    FOREIGN key ("id_estudiante") REFERENCES "estudiante" ("id")
 );
 CREATE TABLE IF NOT EXISTS "revision" (
     "id" INTEGER PRIMARY KEY,
+    -- campo agregado
+    "estado" TEXT COMMENT 'Aprobado o Reprobado',
+    -- campo agregado
     "fecha_entrega_alumno" TEXT COMMENT 'fecha alumno entrega su tesis',
     "fecha_entrega_tribunal" TEXT COMMENT 'fecha se entrega el trabajo a los tribunales',
     "fecha_limite_devolucion" TEXT COMMENT 'fecha limite para que el tribunal devuelva el trabajo revisado',
     "fecha_devolucion_tribunal" TEXT COMMENT 'fecha el tribunal devuelve el trabajo revisado',
     "fecha_devolucion_alumno" TEXT COMMENT 'fecha se entrega la resvision al alumno',
+    --campo agregado
+    "observacion" TEXT COMMENT 'comentarios que puede poner el usuario para la revisión',
     "nro_tribunal" INTEGER COMMENT '1 para tribunal 1, 2 para tribunal 2' NOT NULL,
     "nro_revision" INTEGER COMMENT 'revision 1, 2, 3...' NOT NULL,
     "fecha_empaste" TEXT COMMENT 'si no hay empaste es NULL',
@@ -105,14 +104,13 @@ CREATE TABLE IF NOT EXISTS "detalle_revision" (
     "id" INTEGER PRIMARY KEY,
     "id_revision" INTEGER NOT NULL,
     "id_licenciado" INTEGER NOT NULL,
-    "id_tipo_licenciado" INTEGER,
+    "id_funcion_licenciado" INTEGER,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     CONSTRAINT fk_revision_revisionlicenciado FOREIGN KEY("id_revision") REFERENCES "revision"("id"),
     CONSTRAINT fk_licenciado_revisionlicenciado FOREIGN KEY("id_licenciado") REFERENCES "licenciado"("id"),
-    FOREIGN key ("id_tipo_licenciado") REFERENCES "tipo_licenciado"("id")
+    FOREIGN key ("id_funcion_licenciado") REFERENCES "funcion_licenciado"("id")
 );
-
 CREATE TABLE IF NOT EXISTS "tipo_titulacion_otros"(
     "id" INTEGER PRIMARY KEY,
     "tipo" TEXT COMMENT 'examen de grado, graduación por excelencia',
@@ -134,7 +132,7 @@ CREATE TABLE IF NOT EXISTS "defensa_externa" (
     "fecha_presentacion" TEXT NOT NULL,
     "hora" TEXT NOT NULL,
     "aula" TEXT NOT NULL,
-    "id_tesis" INTEGER ,
+    "id_tesis" INTEGER,
     "id_titulacion_otro" INTEGER,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -145,11 +143,11 @@ CREATE TABLE IF NOT EXISTS "detalle_defensa" (
     "id" INTEGER PRIMARY KEY,
     "id_defensa_externa" INTEGER NOT NULL,
     "id_licenciado" INTEGER NOT NULL,
-    "id_tipo_licenciado" INTEGER NOT NULL,
+    "id_funcion_licenciado" INTEGER NOT NULL,
     "creado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     "modificado_en" TEXT DEFAULT CURRENT_TIMESTAMP NOT NULL,
     FOREIGN KEY("id_licenciado") REFERENCES "licenciado"("id"),
-    FOREIGN KEY("id_tipo_licenciado") REFERENCES "tipo_licenciado"("id"),
+    FOREIGN KEY("id_funcion_licenciado") REFERENCES "funcion_licenciado"("id"),
     FOREIGN KEY("id_defensa_externa") REFERENCES "defensa_externa"("id")
 );
 INSERT INTO facultad (nombre)
@@ -159,4 +157,4 @@ VALUES ('Ciencias y Tecnología');
 INSERT INTO facultad (nombre)
 VALUES ('Ciencias Jurídicas, Sociales');
 INSERT INTO carrera (nombre, id_facultad)
-VALUES ('carrera1', 1); 
+VALUES ('carrera1', 1);
